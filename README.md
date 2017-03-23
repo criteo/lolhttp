5c4ba218275db6a41befdb2831480d05a08b297c
Guillaume Bort <g.bort@criteo.com>
2017-03-23 17:11:34 +0100

    README
    Change-Id: I7ef7146edb2a8cfb939b5233eb1c452a4243e329

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# lol
+# lolhttp
+
+A scala HTTP server & client library.
+
+## About
+
+Servers and clients are service functions. A service takes an HTTP request and eventually returns an HTTP response. Requests and responses are seen as a set of HTTP headers and a content body. The content body is a lazy stream of bytes based on [fs2](https://github.com/functional-streams-for-scala/fs2), making it easy to handle streaming scenarios if needed. Yet, the library provides content encoders and decoders for the common scala types. All concepts are shared between servers and clients, making it simple to assemble them. For example writing a reverse proxy is just a matter of piping a client into a server. SSL is supported on both sides.
+
+## Usage
+
+The library is cross-built for __Scala 2.11__ and __Scala 2.12__. The core module to use is `"com.criteo.lolhttp" %% "lolhttp" % "0.1.0"`.
+
+There are also 2 optional companion libraries:
+
+- `"com.criteo.lolhttp" %% "loljson" % "0.1.0"`, provides integration with the [circe](https://circe.github.io/circe/) JSON library.
+- `"com.criteo.lolhttp" %% "lolhtml" % "0.1.0"`, provides minimal HTML templating.
+
+## Documentation
+
+The [API documentation](http://g.bort.gitlab.preprod.crto.in/lolhttp/api/lol/index.html) is the main reference.
+
+You can also follow this hands-on introduction using annotated example programs:
+
+- [Hello world!](http://g.bort.gitlab.preprod.crto.in/lolhttp/examples/HelloWorld.scala.html)
+- [Serving files from classpath](http://g.bort.gitlab.preprod.crto.in/lolhttp/examples/ServingFiles.scala.html)
+- [Reading large request streams](http://g.bort.gitlab.preprod.crto.in/lolhttp/examples/LargeFileUpload.scala.html)
+- [A simple reverse proxy](http://g.bort.gitlab.preprod.crto.in/lolhttp/examples/ReverseProxy.scala.html)
--- examples/src/main/scala/ServingFiles.scala
+++ examples/src/main/scala/ServingFiles.scala
@@ -1,4 +1,9 @@
-// Example: Serving files
+// Example: Serving files from classpath
+//
+// This example show how to server static content files from
+// the classpath. This is a common pattern to embed your web
+// applications resources (html, js, css, etc.) into your application
+// jar file, and to serve them on a __/public__ endpoint.
 import lol.http._
 import lol.html._
 
@@ -9,6 +14,24 @@ object ServingFiles {
   def main(args: Array[String]): Unit = {
 
     Server.listen(8888) {
+      // Here we specifically serve our favicon image from the __/favicon__
+      // endpoint. We do not need to check if the `ClasspathResource` actually
+      // exists because we know statically that it exists.
+      case GET at url"/favicon" =>
+        Ok(ClasspathResource("/public/icon.gif"))
+
+      // Here we dynamically extract the resource name to serve from the request URL.
+      // We use __fold__ to handle the case where the resource does not exist. We have
+      // to do that because the client could ask for a non existing resource and we want
+      // to send a __404__ response in this case.
+      //
+      // Using `ClasspathResource` here is safe because it is not possible to navigate outside
+      // of the __/public__ directory.
+      //
+      // Also, note how it is possible to use a `ClasspathResource` as content for a response.
+      case GET at url"/assets/$file" =>
+        ClasspathResource(s"/public/$file").fold(NotFound)(r => Ok(r))
+
       case GET at url"/" =>
         Ok(html"""
           <head>
@@ -28,12 +51,6 @@ object ServingFiles {
             <a href="/assets/../secure.txt">/assets/../secure.txt</a>
           </body>
         """)
-
-      case GET at url"/favicon" =>
-        Ok(ClasspathResource("/public/icon.gif"))
-
-      case GET at url"/assets/$file" =>
-        ClasspathResource(s"/public/$file").fold(NotFound)(r => Ok(r))
 
       case _ =>
         NotFound

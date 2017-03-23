1202bd39dfb27e6e51770f97204a8323e39c7024
Guillaume Bort <g.bort@criteo.com>
2017-03-23 15:29:17 +0100

    New website
    Change-Id: Iec8c5d23659f66fc0b93c1104a46a20224aeb240

--- build.sh
+++ build.sh
@@ -9,5 +9,6 @@ cp -rf target/scala-2.12/unidoc api
 cp -rf examples/target/html examples0
 rm -rf examples
 cp -rf examples0 examples
+rm -rf examples0
 git commit -a -m "New website"
 git push gitlab-preprod gh-pages

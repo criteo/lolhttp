286ff3e1d661a6f942a49d7529917672d41a0e0e
Guillaume Bort <g.bort@criteo.com>
2017-07-04 14:53:44 +0200

    Fix binary compatibility problem with scala 2.11

--- core/src/main/scala/SSL.scala
+++ core/src/main/scala/SSL.scala
@@ -3,19 +3,24 @@ package lol.http
 import java.security.{ KeyStore }
 import javax.net.ssl.{ TrustManagerFactory }
 
+import io.netty.buffer.{ ByteBufAllocator }
 import io.netty.handler.ssl.{ SslContext, SslContextBuilder }
 import io.netty.handler.ssl.util.{ InsecureTrustManagerFactory, SelfSignedCertificate }
 
 /** lol SSL. */
 object SSL {
 
+  private[http] class NettySslContext(private val ctx: SslContext) {
+    def newHandler(alloc: ByteBufAllocator) = ctx.newHandler(alloc)
+  }
+
   /** SSL configuration for clients.  */
-  class ClientConfiguration private[http] (private[http] val ctx: SslContext, name: String) {
+  class ClientConfiguration private[http] (private[http] val ctx: NettySslContext, name: String) {
     override def toString = s"ClientConfiguration($ctx, $name)"
   }
 
   /** SSL configuration for servers.  */
-  class ServerConfiguration private[http] (private[http] val ctx: SslContext, name: String) {
+  class ServerConfiguration private[http] (private[http] val ctx: NettySslContext, name: String) {
     override def toString = s"ServerConfiguration($ctx, $name)"
   }
 
@@ -25,7 +30,7 @@ object SSL {
     implicit lazy val default = new ClientConfiguration({
       val trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm)
       trustManagerFactory.init(null: KeyStore)
-      SslContextBuilder.forClient.trustManager(trustManagerFactory).build()
+      new NettySslContext(SslContextBuilder.forClient.trustManager(trustManagerFactory).build())
     }, "default")
   }
 
@@ -34,7 +39,7 @@ object SSL {
     * insecure server.
     */
   lazy val trustAll = new ClientConfiguration({
-    SslContextBuilder.forClient.trustManager(InsecureTrustManagerFactory.INSTANCE).build()
+    new NettySslContext(SslContextBuilder.forClient.trustManager(InsecureTrustManagerFactory.INSTANCE).build())
   }, "trustAll")
 
   /** Generate an SSL server configuration with a self-signed certificate.
@@ -43,7 +48,7 @@ object SSL {
     */
   def selfSigned(fqdn: String = "localhost") = new ServerConfiguration({
     val ssc = new SelfSignedCertificate(fqdn)
-    SslContextBuilder.forServer(ssc.certificate, ssc.privateKey).build()
+    new NettySslContext(SslContextBuilder.forServer(ssc.certificate, ssc.privateKey).build())
   }, s"selfSigned for $fqdn")
 
 }
